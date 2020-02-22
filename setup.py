0d8872b65e533bb5f9a984693313d91fbe0947fe
tranvinhhung99 <xing@cinnamon.is>
2020-02-22 12:11:18 +0700

    add gitignore and setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import os
+
+
+setup(
+   name='beat_pd',
+   version='1.0',
+   description='Utils module used in BeatPD Competition',
+   author='Man Foo',
+   author_email='tvhung@selab.hcmus.edu.vn',
+   packages=['beat_pd'],  #same as name
+   install_requires=[
+       'pandas', 
+        'numpy',
+        'torch',
+        
+    ], #external packages as dependencies
+)
